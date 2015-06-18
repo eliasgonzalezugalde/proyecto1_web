9bc1c326efb74393bd895672669c5a5479b6a762
Elias <eliasgonzalezugalde@gmail.com>
2015-06-18 11:08:13 +0000

    Menu responsive (480px)
    Agregué el CSS para hacer el responsive

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<title>chamberos.net</title>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<link rel="stylesheet" href="css/style.css" />
@@ -18,16 +18,17 @@
 		<div class="page-wrapper">
 			<a id="link_logo" href="main.php"><img class="normal" src="img/logo.png" alt="Logo"></a>
 			<nav id="nav">
-				<ul>
+				<ul class="toAnimate">
 					<li><a href="clients.php">Clients</a></li>
 					<li><a href="chambas.php">Chambas</a></li>
 					<li><a href="invoices.php">Invoices</a></li>
 					<li id="user"><a href="profile.php">user   <i class="fa fa-user"></i></a>
-					<ul>
+					<ul id="sub">
 						<li><a href="index.php">Logout</a></li>
 					</ul>
 					</li>
 				</ul>
+				<a class="asd" id="pull" href="#"><img class="normal" src="img/logo.png" alt="Logo">Menu</a>
 			</nav>
 		</div>
 	</header>
--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ $(function() {
           $(this).addClass("active");
      })
 });
+
+
+$(function() {
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
+ 
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+});
+ 
+$(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+});
