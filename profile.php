18ae9383b82981cbd1aa8973f4748b31049de0bc
Elias <eliasgonzalezugalde@gmail.com>
2015-07-01 22:02:46 +0000

    arreglo a profile

--- profile.php
+++ profile.php
@@ -3,6 +3,13 @@
 <div class="page-wrapper">
 	<section id="contenedor">
 		<section class="col_titulo"><i class="fa fa-user"></i><h2>Profile</h2></section>
+		<form action="main.php" method="POST">
+			<input value='user' type="text" name="name">
+			<input placeholder='pass' type="password" name="psw">
+			<input placeholder='repeat pass' type="password" name="psw">
+			<input value='user' type="submit" value="Save">
+			<a href="chambas.php">Cancel</a>
+		</form>
 	</section>
 </div>
 
