26a9211e227862d841c9cb3f23dc6891694ac41a
Elias <eliasgonzalezugalde@gmail.com>
2015-08-17 13:47:07 +0200

    Arreglo de edits
    quitar el codigo php de los edits

--- edit_chamba.php
+++ edit_chamba.php
@@ -1,18 +1,18 @@
 <?php include("header.php"); ?> 
-<?php $nombre = $_GET["nombre"]; $nota = $_GET["nota"];  $des = $_GET["des"];  $fecha = $_GET["fecha"]; ?>
 
 <div class="page-wrapper">
 	<section id="contenedor">
 		<section class="col_titulo"><i class="fa fa-pencil"></i><h2>Edit Chamba</h2><a href="new_chamba.php" class="btn_new">New Chamba</a></section>
+
 		<form action="saved_chamba.php" method="POST">
-			<input value='<?php echo $nombre; ?>' name="clients" placeholder="Clients" list="clientes">
+			<input value='' name="clients" placeholder="Clients" list="clientes">
 			<datalist id="clientes">
   				<option value="Elías González">
   				<option value="Bladimir Arroyo">
 			</datalist>
-			<textarea name="description" placeholder="Description" cols="28"><?php echo $des; ?></textarea>
-			<input value='<?php echo $fecha; ?>' type="date" name="date">
-			<textarea name="notes" placeholder="Notes" cols="28"><?php echo $nota; ?></textarea>
+			<textarea name="description" placeholder="Description" cols="28"></textarea>
+			<input value='' type="date" name="date">
+			<textarea name="notes" placeholder="Notes" cols="28"></textarea>
 			<input type="submit" value="Save">
 			<a href="chambas.php">Cancel</a>
 		</form>
--- edit_client.php
+++ edit_client.php
@@ -1,13 +1,13 @@
 <?php include("header.php"); ?> 
-<?php $nombre = $_GET["nombre"]; $id = $_GET["id"]; $tel = $_GET["tel"]; $nombreArray = explode(" ", $nombre); ?>
+
 <div class="page-wrapper">
 	<section id="contenedor">
 		<section class="col_titulo"><i class="fa fa-pencil"></i><h2>Edit Client</h2><a href="new_client.php" class="btn_new">New Client</a></section>
 		<form action="saved_client.php" method="POST">
-			<input type="text" value='<?php echo $nombreArray[0]; ?>' placeholder="Name" name="name" id="name">
-			<input type="text" value='<?php echo $nombreArray[1]; ?>' placeholder="Last Name" name="last_name" id="lastName">
-			<input type="text" value='<?php echo $id; ?>' placeholder="Id" name="id" id="id">
-			<input type="tel" pattern="[0-9]{10}" value='<?php echo $tel; ?>' placeholder="Phone" name="phone" id="phone">
+			<input type="text" value='' placeholder="Name" name="name" id="name">
+			<input type="text" value='' placeholder="Last Name" name="last_name" id="lastName">
+			<input type="text" value='' placeholder="Id" name="id" id="id">
+			<input type="tel" pattern="[0-9]{10}" value='' placeholder="Phone" name="phone" id="phone">
 			<input type="submit" value="Save" id="editClient">
 			<a href="clients.php">Cancel</a>
 		</form>
